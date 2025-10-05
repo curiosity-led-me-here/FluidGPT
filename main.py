a54a14ad5fa24b7d1adf8583c9fb7b7ccc6ca638
morphed_human <163374239+curiosity-led-me-here@users.noreply.github.com>
2025-10-05 18:50:31 +0530

    start data feeding

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import torch.optim as optim
 import torch
 from model import positional_encoding, GPT
 
+with open("data.txt") as file:
+    data = file.read()
+
+encoding = tiktoken.encoding_for_model("gpt-4")
+tokens = encoding.encode(data)
+print("Token count:", len(tokens))
+
